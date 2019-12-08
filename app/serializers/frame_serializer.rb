8f1624ea22739da3396569a3368ed5538b3073f4
Ditjon Berisha <ditjonberisha@gmail.com>
2019-12-08 20:10:13 +0100

    Annotated frame serializer

--- app/serializers/frame_serializer.rb
+++ app/serializers/frame_serializer.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: frames
+#
+#  id          :bigint           not null, primary key
+#  first_ball  :integer
+#  number      :integer
+#  second_ball :integer
+#  status      :integer          default("normal")
+#  third_ball  :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  game_id     :bigint
+#
+# Indexes
+#
+#  index_frames_on_game_id  (game_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (game_id => games.id)
+#
+
 class FrameSerializer < ActiveModel::Serializer
   attributes :number, :first_ball, :second_ball, :third_ball, :status, :score
 end
