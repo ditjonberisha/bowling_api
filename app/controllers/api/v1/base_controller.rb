af72d3f660155d129e226067c4780ec64459ea90
Ditjon Berisha <ditjonberisha@gmail.com>
2019-12-08 20:00:56 +0100

    Added game routes

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,18 @@
 class Api::V1::BaseController < ApplicationController
   include Errors::ErrorHandler
   include Helpers::Render
+
+  def api
+    render json: api_json, status: 200
+  end
+
+  private
+    def api_json
+      {
+          status: :ok,
+          new_game_url: api_v1_games_url,
+          new_points_url: api_v1_game_points_url(':game_id', ':points'),
+          game_score_url: api_v1_game_url(':id')
+      }
+    end
 end
