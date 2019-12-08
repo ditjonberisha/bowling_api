04cf0d25e2b00b2c9a2e71475a6b6ad09498721e
Ditjon Berisha <ditjonberisha@gmail.com>
2019-12-08 19:10:47 +0100

    Create rspec for games/base_controller

--- spec/controllers/api/v1/games/base_controller_spec.rb
+++ spec/controllers/api/v1/games/base_controller_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Games::BaseController, type: :controller do
+  describe 'before action' do
+    it 'should call set game' do
+      should use_before_action(:set_game)
+    end
+  end
 end
